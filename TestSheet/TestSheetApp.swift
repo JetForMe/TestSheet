2bc924bbb5b7a31c14f4ca7cca3b583055ee27a1
Rick M <rmann@latencyzero.com>
2023-02-14 18:52:31 -0800

    Initial Commit

---
+++ TestSheet/TestSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSheetApp.swift
+//  TestSheet
+//
+//  Created by Rick Mann on 2023-02-14.
+//
+
+import SwiftUI
+
+@main
+struct TestSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
