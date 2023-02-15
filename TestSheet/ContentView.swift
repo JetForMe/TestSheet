499416b8852a4fe4e11bc75473530451a482a213
Rick M <rmann@latencyzero.com>
2023-02-14 19:10:35 -0800

    First commit.

--- TestSheet/ContentView.swift
+++ TestSheet/ContentView.swift
@@ -9,14 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationSplitView
+		{
+			List
+			{
+				Text("Foo")
+				Text("Bar")
+			}
+			.navigationSplitViewColumnWidth(min: 200, ideal: 200)
+		}
+		detail:
+		{
+			NavigationStack
+			{
+				Text("The Detail")
+					.navigationTitle("Detail")
+					.toolbar
+					{
+						ToolbarItemGroup
+						{
+							NavigationLink(value: "New thing")
+							{
+								Label("New thing", systemImage: "plus")
+							}
+						}
+					}
+					.navigationDestination(for: String.self)
+					{ _ in
+						AddView()
+					}
+			}
+		}
     }
+}
+
+struct
+AddView : View
+{
+	var
+	body: some View
+	{
+		Text("new thing editor")
+			.navigationTitle("New Thing")
+			.toolbar
+			{
+				ToolbarItemGroup
+				{
+					Button(action: { self.isPresentingSearch = true })
+					{
+						Label("Search Thing", systemImage: "magnifyingglass")
+					}
+				}
+			}
+			.sheet(isPresented: self.$isPresentingSearch)
+			{
+				Text("Search Thing UI")
+			}
+	}
+	
+	@State	private	var	isPresentingSearch					=	false
 }
 
 struct ContentView_Previews: PreviewProvider {
